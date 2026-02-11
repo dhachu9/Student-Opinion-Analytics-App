3836f7b64e22d4136de040d2874cecbf8c8c991f
dhachu9 <dhanva999@gmail.com>
2026-02-11 11:04:17 +0530

    Create TeacherDashboardScreen.js

---
+++ frontend/src/screens/TeacherDashboardScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function TeacherDashboardScreen() {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text>Teacher Analytics Dashboard</Text>
+    </View>
+  );
+}
